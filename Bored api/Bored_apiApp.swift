3927064f1c49521c024eeddd64b0305daf7c03d8
bfddeveloper <dicksonbrody@icloud.com>
2023-02-27 09:33:04 -0600

    Initial Commit

---
+++ Bored api/Bored_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bored_apiApp.swift
+//  Bored api
+//
+//  Created by Brody Dickson on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Bored_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
